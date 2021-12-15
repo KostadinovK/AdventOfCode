1b2cb6b3ead7674a795d345a38878072272beb9a
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2021-12-15 15:25:13 +0200

    Add solution for 14-12 and a slow solution for 15-12(needs rewriting)

---
+++ AdventOfCode/14-12/Program.cs
@@ -0,0 +1,61 @@
+﻿var path = Directory.GetCurrentDirectory();
+var lines = File.ReadAllLines(@$"{path}\input.txt");
+
+var countsByPair = new Dictionary<string, long>();
+var pairInsertionRules = new Dictionary<string, char>();
+
+var polymer = lines[0];
+
+for (int i = 0; i < polymer.Length - 1; i++)
+{
+    var pair = "" + polymer[i] + polymer[i + 1];
+
+    if (!countsByPair.ContainsKey(pair))
+    {
+        countsByPair.Add(pair, 0);
+    }
+
+    countsByPair[pair]++;
+}
+
+for (int i = 2; i < lines.Length; i++)
+{
+    var instruction = lines[i];
+    var pair = instruction.Split(" -> ")[0];
+    var newChar = instruction.Split(" -> ")[1].ToCharArray()[0];
+
+    if (!pairInsertionRules.ContainsKey(pair))
+    {
+        pairInsertionRules.Add(pair, ' ');
+    }
+
+    pairInsertionRules[pair] = newChar;
+}
+
+for (int step = 0; step < 40; step++)
+{
+    var newCountsByPair = new Dictionary<string, long>();
+
+    foreach (var kvp in countsByPair)
+    {
+        var (pair, count) = kvp;
+        var (a, n, b) = (pair[0], pairInsertionRules[pair], pair[1]);
+
+        newCountsByPair[$"{a}{n}"] = newCountsByPair.GetValueOrDefault($"{a}{n}") + count;
+        newCountsByPair[$"{n}{b}"] = newCountsByPair.GetValueOrDefault($"{n}{b}") + count;
+    }
+
+    countsByPair = newCountsByPair;
+}
+
+var countsByLetter = new Dictionary<char, long>();
+
+foreach (var (molecule, count) in countsByPair)
+{
+    var a = molecule[0];
+    countsByLetter[a] = countsByLetter.GetValueOrDefault(a) + count;
+}
+
+countsByLetter[polymer.Last()]++;
+
+Console.WriteLine(countsByLetter.Values.Max() - countsByLetter.Values.Min());
---
+++ AdventOfCode/15-12/Program.cs
@@ -0,0 +1,67 @@
+﻿var path = Directory.GetCurrentDirectory();
+var lines = File.ReadAllLines(@$"{path}\input.txt");
+
+var cavern = new int[lines.Length, lines[0].Length];
+var visited = new bool[lines.Length, lines[0].Length];
+
+for (var i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+
+    for (var j = 0; j < line.Length; j++)
+    {
+        var risk = line[j] - '0';
+
+        cavern[i, j] = risk;
+    }
+}
+
+var pathsRisk = new List<int>();
+
+GetAllPathsRisk(0, 0, 0);
+
+var pathWithLowestRisk = pathsRisk.Min();
+
+Console.WriteLine(pathWithLowestRisk);
+
+int GetAllPathsRisk(int row, int col, int risk)
+{
+    if (row < 0 || row >= cavern.GetLength(0) || col < 0 || col >= cavern.GetLength(1))
+    {
+        return risk;
+    }
+
+    if (visited[row, col])
+    {
+        return risk;
+    }
+
+    var value = cavern[row, col];
+
+    if (row == cavern.GetLength(0) - 1 && col == cavern.GetLength(1) - 1)
+    {
+        pathsRisk.Add(risk + value);
+        return risk;
+    }
+
+    if (row != 0 || col != 0)
+    {
+        risk += value;
+    }
+
+    visited[row, col] = true;
+
+    risk = GetAllPathsRisk(row - 1, col, risk);
+    risk = GetAllPathsRisk(row + 1, col, risk);
+    risk = GetAllPathsRisk(row, col - 1, risk);
+    risk = GetAllPathsRisk(row, col + 1, risk);
+
+    visited[row, col] = false;
+
+    if (row != 0 || col != 0)
+    {
+        risk -= value;
+    }
+
+    return risk;
+}
