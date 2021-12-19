924ac61d72db2a3d93506846959beaa1010e0856
Kostadin Kostadinov <kostadinovk2001@gmail.com>
2021-12-19 13:26:48 +0200

    Add solution for 15-12

---
+++ AdventOfCode/15-12/Point.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_12
+{
+    public record Point(int x, int y);
+}
--- AdventOfCode/15-12/Program.cs
+++ AdventOfCode/15-12/Program.cs
@@ -1,8 +1,9 @@
-﻿var path = Directory.GetCurrentDirectory();
+﻿using _15_12;
+
+var path = Directory.GetCurrentDirectory();
 var lines = File.ReadAllLines(@$"{path}\input.txt");
 
-var cavern = new int[lines.Length, lines[0].Length];
-var visited = new bool[lines.Length, lines[0].Length];
+var map = new Dictionary<Point, int>();
 
 for (var i = 0; i < lines.Length; i++)
 {
@@ -10,58 +11,92 @@ for (var i = 0; i < lines.Length; i++)
 
     for (var j = 0; j < line.Length; j++)
     {
-        var risk = line[j] - '0';
+        var point = new Point(j, i);
 
-        cavern[i, j] = risk;
+        if (!map.ContainsKey(point))
+        {
+            map[point] = 0;
+        }
+
+        map[point] = line[j] - '0';
     }
 }
 
-var pathsRisk = new List<int>();
+var part1Result = GetLowestRiskFromStartToEnd(map);
 
-GetAllPathsRisk(0, 0, 0);
+var scaledMap = ScaleUp(map);
 
-var pathWithLowestRisk = pathsRisk.Min();
+var part2Result = GetLowestRiskFromStartToEnd(scaledMap);
 
-Console.WriteLine(pathWithLowestRisk);
+Console.WriteLine(part1Result);
+Console.WriteLine(part2Result);
 
-int GetAllPathsRisk(int row, int col, int risk)
+//Dijkstra
+int GetLowestRiskFromStartToEnd(Dictionary<Point, int> map)
 {
-    if (row < 0 || row >= cavern.GetLength(0) || col < 0 || col >= cavern.GetLength(1))
+    var start = new Point(0, 0);
+    var end = new Point(map.Keys.MaxBy(p => p.x).x, map.Keys.MaxBy(p => p.y).y);
+
+    var queue = new PriorityQueue<Point, int>();
+    var totalRiskMap = new Dictionary<Point, int>();
+
+    totalRiskMap[start] = 0;
+    queue.Enqueue(start, 0);
+
+    while (true)
     {
-        return risk;
+        var p = queue.Dequeue();
+
+        if (p == end)
+        {
+            break;
+        }
+
+        foreach (var n in GetNeighbours(p))
+        {
+            if (map.ContainsKey(n))
+            {
+                var totalRiskThroughP = totalRiskMap[p] + map[n];
+                if (totalRiskThroughP < totalRiskMap.GetValueOrDefault(n, int.MaxValue))
+                {
+                    totalRiskMap[n] = totalRiskThroughP;
+                    queue.Enqueue(n, totalRiskThroughP);
+                }
+            }
+        }
     }
 
-    if (visited[row, col])
-    {
-        return risk;
-    }
+    return totalRiskMap[end];
+}
 
-    var value = cavern[row, col];
+IEnumerable<Point> GetNeighbours(Point point) =>
+    new[] {
+        point with {y = point.y + 1},
+        point with {y = point.y - 1},
+        point with {x = point.x + 1},
+        point with {x = point.x - 1},
+    };
 
-    if (row == cavern.GetLength(0) - 1 && col == cavern.GetLength(1) - 1)
-    {
-        pathsRisk.Add(risk + value);
-        return risk;
-    }
+Dictionary<Point, int> ScaleUp(Dictionary<Point, int> map)
+{
+    var (ccol, crow) = (map.Keys.MaxBy(p => p.x).x + 1, map.Keys.MaxBy(p => p.y).y + 1);
 
-    if (row != 0 || col != 0)
-    {
-        risk += value;
-    }
+    var res = new Dictionary<Point, int>(
+        from y in Enumerable.Range(0, crow * 5)
+        from x in Enumerable.Range(0, ccol * 5)
 
-    visited[row, col] = true;
+        // x, y and risk level in the original map:
+        let tileY = y % crow
+        let tileX = x % ccol
+        let tileRiskLevel = map[new Point(tileX, tileY)]
 
-    risk = GetAllPathsRisk(row - 1, col, risk);
-    risk = GetAllPathsRisk(row + 1, col, risk);
-    risk = GetAllPathsRisk(row, col - 1, risk);
-    risk = GetAllPathsRisk(row, col + 1, risk);
+        // risk level is increased by tile distance from origin:
+        let tileDistance = (y / crow) + (x / ccol)
 
-    visited[row, col] = false;
+        // risk level wraps around from 9 to 1:
+        let riskLevel = (tileRiskLevel + tileDistance - 1) % 9 + 1
+        select new KeyValuePair<Point, int>(new Point(x, y), riskLevel)
+    );
 
-    if (row != 0 || col != 0)
-    {
-        risk -= value;
-    }
-
-    return risk;
+    return res;
 }
