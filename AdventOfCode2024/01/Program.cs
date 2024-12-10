632c7b7f83192d2fe12a59b6f701723c5541767f
Kostadin Kostadinov <Kostadin.Kostadinov@adesso.bg>
2024-12-10 13:43:13 +0200

    2024: add solutions for day 1 and partial solution for day 2

---
+++ AdventOfCode2024/01/Program.cs
@@ -0,0 +1,76 @@
+﻿var path = Directory.GetCurrentDirectory();
+var lines = File.ReadAllLines(@$"{path}\input.txt");
+
+var leftNumbers = new List<int>();
+var rightNumbers = new List<int>();
+
+foreach (var line in lines)
+{
+    var numbers = line.Split(' ');
+    numbers = numbers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+    var firstNumber = int.Parse(numbers[0]);
+    var secondNumber = int.Parse(numbers[1]);
+
+    leftNumbers.Add(firstNumber);
+    rightNumbers.Add(secondNumber);
+}
+
+if (leftNumbers.Count != rightNumbers.Count)
+{
+    throw new ArgumentOutOfRangeException("The two lists must have the same count");
+}
+
+
+
+SolveTotalDistanceSum(leftNumbers, rightNumbers);
+SolveSimilarityScore(leftNumbers, rightNumbers);
+return;
+
+void SolveTotalDistanceSum(List<int> leftList, List<int> rightList)
+{
+    leftList.Sort();
+    rightList.Sort();
+
+    long distanceSum = 0;
+    for (int i = 0; i < leftList.Count; i++)
+    {
+        var distanceBetweenNumbers = Math.Abs(leftList[i] - rightList[i]);
+        distanceSum += distanceBetweenNumbers;
+    }
+
+    Console.WriteLine($"Total Distance Sum: {distanceSum}");
+}
+
+void SolveSimilarityScore(List<int> leftList, List<int> rightList)
+{
+    var leftNumbersOcurencesInRightList = new Dictionary<int, int>();
+
+    foreach (var leftNum in leftList)
+    {
+        if (leftNumbersOcurencesInRightList.ContainsKey(leftNum))
+        {
+            continue;
+        }
+
+        var occurrences = 0;
+        foreach (var rightNum in rightList)
+        {
+            if (leftNum == rightNum)
+            {
+                occurrences++;
+            }
+        }
+
+        leftNumbersOcurencesInRightList.Add(leftNum, occurrences);
+    }
+
+    long similarityScore = 0;
+
+    foreach (var (number, numberOfOccurrences) in leftNumbersOcurencesInRightList)
+    {
+        similarityScore += number * numberOfOccurrences;
+    }
+
+    Console.WriteLine($"Total Similarity Score: {similarityScore}");
+}
---
+++ AdventOfCode2024/02/Program.cs
@@ -0,0 +1,111 @@
+﻿var path = Directory.GetCurrentDirectory();
+var lines = File.ReadAllLines(@$"{path}\input.txt");
+
+var reports = new List<List<int>>();
+foreach (var line in lines)
+{
+    var report = new List<int>();
+    var numbers = line.Split(' ');
+
+    foreach (var number in numbers)
+    {
+        report.Add(int.Parse(number));
+    }
+
+    reports.Add(report);
+}
+
+DisplaySafeRepostsCount(reports);
+DisplaySafeRepostsCountWithProblemDampener(reports);
+
+void DisplaySafeRepostsCountWithProblemDampener(List<List<int>> reports)
+{
+    var safeReports = 0;
+
+    foreach (var report in reports)
+    {
+        var reportIsSafe = true;
+        var levelRemovedCount = 0;
+        var prevLevelsDiff = Int32.MaxValue;
+        for (int i = 0; i < report.Count - 1; i++)
+        {
+            var currLevel = report[i];
+            var nextLevel = report[i + 1];
+
+            var diff = currLevel - nextLevel;
+
+            if (diff == 0 || diff < -3 || diff > 3)
+            {
+                levelRemovedCount++;
+                if (levelRemovedCount > 1)
+                {
+                    reportIsSafe = false;
+                    break;
+                }
+                report[i + 1] = currLevel;
+                continue;
+            }
+
+            if (prevLevelsDiff != Int32.MaxValue && Math.Sign(prevLevelsDiff) != Math.Sign(diff))
+            {
+                levelRemovedCount++;
+                if (levelRemovedCount > 1)
+                {
+                    reportIsSafe = false;
+                    break;
+                }
+                report[i + 1] = currLevel;
+                continue;
+            }
+
+            prevLevelsDiff = diff;
+        }
+
+        if (reportIsSafe)
+        {
+            safeReports++;
+        }
+    }
+
+    Console.WriteLine($"Safe reports count with Problem Damper: {safeReports}");
+}
+
+
+void DisplaySafeRepostsCount(List<List<int>> reports)
+{
+    var safeReports = 0;
+
+    foreach (var report in reports)
+    {
+        var reportIsSafe = true;
+        var prevLevelsDiff = Int32.MaxValue;
+        for (int i = 0; i < report.Count - 1; i++)
+        {
+            var currLevel = report[i];
+            var nextLevel = report[i + 1];
+
+            var diff = currLevel - nextLevel;
+
+            if (diff == 0 || diff < -3 || diff > 3)
+            {
+                reportIsSafe = false;
+                break;
+            }
+
+            if (prevLevelsDiff != Int32.MaxValue && Math.Sign(prevLevelsDiff) != Math.Sign(diff))
+            {
+                reportIsSafe = false;
+                break;
+            }
+
+            prevLevelsDiff = diff;
+        }
+
+        if (reportIsSafe)
+        {
+            safeReports++;
+        }
+    }
+
+    Console.WriteLine($"Safe reports count: {safeReports}");
+}
