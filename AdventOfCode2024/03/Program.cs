e1ec3ade1f0422f2cf51ee6e277e7a20f56b3337
Kostadin Kostadinov <Kostadin.Kostadinov@adesso.bg>
2024-12-12 17:14:01 +0200

    add solution for day 3

---
+++ AdventOfCode2024/03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+var path = Directory.GetCurrentDirectory();
+var input = File.ReadAllText(@$"{path}\input.txt");
+
+long sum = 0;
+
+var operationsPattern = @"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)";
+var matches = Regex.Matches(input, operationsPattern);
+
+var isMulCommandEnabled = true;
+foreach (Match match in matches)
+{
+    switch (match.Value)
+    {
+        case "do()":
+            isMulCommandEnabled = true;
+            break;
+        case "don't()":
+            isMulCommandEnabled = false;
+            break;
+        default:
+            if (!isMulCommandEnabled)
+            {
+                break;
+            }
+            var firstNumber = int.Parse(match.Groups[1].Value);
+            var secondNumber = int.Parse(match.Groups[2].Value);
+
+            sum += firstNumber * secondNumber;
+            break;
+    }
+}
+
+Console.WriteLine($"Sum: {sum}");
